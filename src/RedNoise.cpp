f065bb0e9c091796bcea2f26754f54ee96eee359
Kejia Zhang <84448985+Kejia928@users.noreply.github.com>
2022-10-03 15:45:40 +0100

    week2

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -1,12 +1,12 @@
 #include <CanvasTriangle.h>
 #include <DrawingWindow.h>
-#include <Utils.h>
-#include <fstream>
 #include <vector>
+#include <glm/glm.hpp>
 
 #define WIDTH 320
 #define HEIGHT 240
 
+// ---------------------- Week 1 ---------------------- //
 void draw(DrawingWindow &window) {
 	window.clearPixels();
 	for (size_t y = 0; y < window.height; y++) {
@@ -18,6 +18,57 @@ void draw(DrawingWindow &window) {
 			window.setPixelColour(x, y, colour);
 		}
 	}
+}
+
+// ---------------------- Week 2 ---------------------- //
+std::vector<float> interpolateSingleFloats(float from, float to, int numberOfValue) {
+    std::vector<float> result;
+    float difference = (to - from) / float(numberOfValue - 1);
+    for(int i=0; i<numberOfValue; i++) result.push_back(from + (float)i*difference);
+    return result;
+}
+
+void greyscaleInterpolation(DrawingWindow &window) {
+    window.clearPixels();
+    std::vector<float> interpolation = interpolateSingleFloats(255, 0, int(window.width));
+    for (size_t x = 0; x < window.width; x++) {
+        float red = interpolation[x];
+        float green = interpolation[x];
+        float blue = interpolation[x];
+        for (size_t y = 0; y < window.height; y++) {
+            uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
+            window.setPixelColour(x, y, colour);
+        }
+    }
+}
+
+std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int numberOfValue) {
+    std::vector<glm::vec3> result;
+    glm::vec3 difference = (to - from) * (1 / (float(numberOfValue) - 1));
+    for(int i=0; i<numberOfValue; i++) {
+        result.push_back(from + difference*float(i));
+    }
+    return result;
+}
+
+void twoDimensionalColourInterpolation(DrawingWindow &window) {
+    window.clearPixels();
+    glm::vec3 topLeft(255, 0, 0);        // red
+    glm::vec3 topRight(0, 0, 255);       // blue
+    glm::vec3 bottomRight(0, 255, 0);    // green
+    glm::vec3 bottomLeft(255, 255, 0);   // yellow
+    std::vector<glm::vec3> left = interpolateThreeElementValues(topLeft, bottomLeft, int(window.height));
+    std::vector<glm::vec3> right = interpolateThreeElementValues(topRight, bottomRight, int(window.width));
+    for(size_t y=0; y<window.height; y++) {
+        std::vector<glm::vec3> row = interpolateThreeElementValues(left[y], right[y], int(window.width));
+        for(size_t x=0; x<window.width; x++) {
+            float red = row[x].x;
+            float green = row[x].y;
+            float blue = row[x].z;
+            uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
+            window.setPixelColour(x, y, colour);
+        }
+    }
 }
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
@@ -33,12 +84,27 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 }
 
 int main(int argc, char *argv[]) {
+     // Single Element Numerical Interpolation
+//     std::vector<float> result;
+//     result = interpolateSingleFloats(2.2, 8.5, 7);
+//     for(float i : result) std::cout << i << " ";
+//     std::cout << std::endl;
+
+    // Three Element Numerical Interpolation
+//     std::vector<glm::vec3> result;
+//     glm::vec3 from(1, 4, 9.2);
+//     glm::vec3 to(4, 1, 9.8);
+//     result = interpolateThreeElementValues(from, to, 4);
+//     for(size_t i=0; i<result.size(); i++) std::cout << result[i].x << " " << result[i].y << " " << result[i].z << " " << std::endl;
+
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
-		draw(window);
+        // draw(window);
+        // greyscaleInterpolation(window);
+        twoDimensionalColourInterpolation(window);
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
